4107461cfa7b07f1e8983cd071d3bc0f0eaa025b
Hosoo Cha <hosoo.cha@gmail.com>
2024-05-22 22:51:25 +1200

    account table styling

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -10,6 +10,7 @@ import Paper from "@mui/material/Paper";
 import Pagination from "@mui/material/Pagination";
 import TablePagination from "@mui/material/TablePagination";
 
+
 function createData(
   order: string,
   date: string,
@@ -40,11 +41,11 @@ const Account = () => {
   const [orderHistory, setOrderHistory] = useState(true);
 
   const [itemsPerPage, setItemsPerPage] = useState(8);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [noOfPages] = useState(Math.ceil(rows.length / itemsPerPage));
 
   const handleChange = (
-    event: React.ChangeEvent<unknown>,
+    event: React.MouseEvent<HTMLButtonElement> | null,
     value: number
   ) => {
     setPage(value);
@@ -62,23 +63,26 @@ const Account = () => {
         <div className="account-flex-wrap-r">
           <h2>Orders</h2>
           <hr></hr>
-          <h3>Order History (0)</h3>
+          <h3>Order History</h3>
           <div className="account-order-history-container">
             {orderHistory ? (
-              <div>
-                <TableContainer component={Paper}>
+              <div className="account-order-history-exists">
+                <TableContainer>
                   <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                       <TableRow>
                         <TableCell>Order</TableCell>
-                        <TableCell align="right">Date</TableCell>
-                        <TableCell align="right">Status</TableCell>
-                        <TableCell align="right">Total</TableCell>
+                        <TableCell align="left">Date</TableCell>
+                        <TableCell align="left">Status</TableCell>
+                        <TableCell align="left">Total</TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
                       {rows
-                        .slice((page - 1) * itemsPerPage, page * itemsPerPage )
+                        .slice(
+                          page * itemsPerPage,
+                          page * itemsPerPage + itemsPerPage
+                        )
                         .map((row) => (
                           <TableRow
                             key={row.order}
@@ -86,23 +90,26 @@ const Account = () => {
                               "&:last-child td, &:last-child th": { border: 0 },
                             }}
                           >
-                            <TableCell component="th" scope="row">
-                              {row.order}
+                            <TableCell component="th" scope="row" style={{cursor: "pointer"}}>
+                              #{row.order}
                             </TableCell>
-                            <TableCell align="right">{row.date}</TableCell>
-                            <TableCell align="right">{row.status}</TableCell>
-                            <TableCell align="right">{row.total}</TableCell>
+                            <TableCell align="left">{row.date}</TableCell>
+                            <TableCell align="left">{row.status}</TableCell>
+                            <TableCell align="left">${row.total}.00</TableCell>
                           </TableRow>
                         ))}
                     </TableBody>
                   </Table>
+
+                    <TablePagination
+                      count={rows.length}
+                      page={page}
+                      rowsPerPage={itemsPerPage}
+                      rowsPerPageOptions={[-1]}
+                      onPageChange={handleChange}
+                      style={{padding: '1rem', display: "flex", border: "none"}}
+                    />
                 </TableContainer>
-                <Pagination
-          count={noOfPages}
-          page={page}
-          onChange={handleChange}
-          defaultPage={1}
-        ></Pagination>
               </div>
             ) : (
               <div className="account-order-history-none">
