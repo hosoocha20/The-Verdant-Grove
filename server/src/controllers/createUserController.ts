c31aabaef5a92738bc447cffd8a423e61d9a7709
Hosoo Cha <hosoo.cha@gmail.com>
2024-06-20 23:00:48 +1200

    added cart to post user

--- server/src/controllers/createUserController.ts
+++ server/src/controllers/createUserController.ts
@@ -1,18 +1,30 @@
 import express, { Request, Response } from "express";
 
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import cors from 'cors';
 import User from '../models/User'
 
+
+
 export async function createUserController(req: Request, res: Response) {
-    const {email, pw, firstName, lastName} = req.body;
+    const { firstName, lastName,email, pw, cart} = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPw = bcrypt.hashSync(pw, salt);
     try{
         const newUser = new User({
             firstName: firstName,
             lastName: lastName,
             email: email,
-            pw: pw
+            pw: hashedPw,
+            cart: cart
         })
+        const token = jwt.sign({ email }, 'secret', {expiresIn: '1hr'})
+        res.json({ email, token})
     }catch(err){
         console.log(err)
+        if (err) {
+            res.json({ detail: 'An account with this email already exists.'})
+          }
     }
 }
