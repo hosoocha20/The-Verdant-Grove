054aad06b755099a761e63b1f01ef4afb0593b82
Hosoo Cha <hosoo.cha@gmail.com>
2024-06-25 18:35:15 +1200

    responsive: login + signup

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState} from "react";
-import axios from "axios";
 import { useCookies } from "react-cookie";
-import { IUser } from "../interfaces/IUser";
 import { IShoppingCartItem } from "../interfaces/IShop";
 import { useOutletContext } from "react-router-dom";
 
