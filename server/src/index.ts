db8434c3c7ef4e2d6d713fe4850ad418694251ee
Hosoo Cha <hosoo.cha@gmail.com>
2024-06-29 00:52:41 +1200

    cors -A

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ app.use(cors(
     {
         origin: ["https://the-verdant-grove.vercel.app"],
         methods:["POST", "GET", "PUT", "DELETE"],
-        allowedHeaders: ['Content-Type', 'authorization'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
     }
 ));
